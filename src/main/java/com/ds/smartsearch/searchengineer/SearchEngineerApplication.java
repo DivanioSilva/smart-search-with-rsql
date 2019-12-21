72cbe1436154ff186476a940704938a00c578b99
Divanio Silva <dcsilva@mail.ru>
2019-12-21 13:05:21 +0000

    First commit

---
+++ src/main/java/com/ds/smartsearch/searchengineer/SearchEngineerApplication.java
@@ -0,0 +1,13 @@
+package com.ds.smartsearch.searchengineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchEngineerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/smartsearch/searchengineer/SearchEngineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.smartsearch.searchengineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
